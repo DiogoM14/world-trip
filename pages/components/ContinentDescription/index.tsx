bd5dc21a48399dd5330ad80b4d8ce475acac12a8
DiogoM14 <diogomartins200214@gmail.com>
2021-05-19 18:40:19 +0100

    Styling

--- pages/components/ContinentDescription/index.tsx
+++ pages/components/ContinentDescription/index.tsx
@@ -1,20 +1,64 @@
-import { Container, Flex, Text, Wrap, WrapItem } from "@chakra-ui/react";
+import { Flex, Text, Wrap, WrapItem } from "@chakra-ui/react";
 
 export function ContinentDescription() {
   return (
     <Flex maxW="1160px">
-      <Wrap>
-        <Text>A Europa é, por convenção, um dos seis continentes do mundo. Compreendendo a península ocidental da Eurásia, a Europa geralmente divide-se da Ásia a leste pela divisória de águas dos montes Urais, o rio Ural, o mar Cáspio, o Cáucaso, e o mar Negro a sudeste</Text>
+      <Wrap maxW="600px">
+        <Text
+          color="gray.700"
+        >
+          A Europa é, por convenção, um dos seis continentes do mundo. Compreendendo a península ocidental da Eurásia, a Europa geralmente divide-se da Ásia a leste pela divisória de águas dos montes Urais, o rio Ural, o mar Cáspio, o Cáucaso, e o mar Negro a sudeste
+        </Text>
       </Wrap>
-      <Wrap>
-        <WrapItem>
-          <Text>50</Text>
-          <Text>Países</Text>
-          <Text>50</Text>
-          <Text>Países</Text>
-          <Text>50</Text>
-          <Text>Países</Text>
-        </WrapItem>
+      <Wrap display="flex" w="100%" justifyContent="space-around">
+          <WrapItem 
+            display="flex" 
+            flexDir="column"
+            align="center"
+          >
+            <Text 
+              color="yellow.900"
+              fontWeight="semibold"
+              fontSize="3xl"
+            >50</Text>
+            <Text
+              color="gray.700"
+              fontWeight="semibold"
+              fontSize="medium"
+            >Países</Text>
+          </WrapItem>
+          <WrapItem 
+            display="flex" 
+            flexDir="column"
+            align="center"
+          >
+            <Text 
+              color="yellow.900"
+              fontWeight="semibold"
+              fontSize="3xl"
+            >50</Text>
+            <Text
+              color="gray.700"
+              fontWeight="semibold"
+              fontSize="medium"
+            >Países</Text>
+          </WrapItem>
+          <WrapItem 
+            display="flex" 
+            flexDir="column"
+            align="center"
+          >
+            <Text 
+              color="yellow.900"
+              fontWeight="semibold"
+              fontSize="3xl"
+            >50</Text>
+            <Text
+              color="gray.700"
+              fontWeight="semibold"
+              fontSize="medium"
+            >Países</Text>
+          </WrapItem>
       </Wrap>
     </Flex>
   )
