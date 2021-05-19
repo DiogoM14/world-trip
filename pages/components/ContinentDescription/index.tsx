8daeb5bc3464758a71c9848471956ec295bb9ec0
DiogoM14 <diogomartins200214@gmail.com>
2021-05-19 18:41:52 +0100

    added grid

--- pages/components/ContinentDescription/index.tsx
+++ pages/components/ContinentDescription/index.tsx
@@ -1,12 +1,12 @@
-import { Container, Flex, Text, Wrap, WrapItem } from "@chakra-ui/react";
+import { Container, Flex, Grid, GridItem, Text, Wrap, WrapItem } from "@chakra-ui/react";
 
 export function ContinentDescription() {
   return (
-    <Flex maxW="1160px">
-      <Wrap>
+    <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+      <GridItem colSpan={2}>
         <Text>A Europa é, por convenção, um dos seis continentes do mundo. Compreendendo a península ocidental da Eurásia, a Europa geralmente divide-se da Ásia a leste pela divisória de águas dos montes Urais, o rio Ural, o mar Cáspio, o Cáucaso, e o mar Negro a sudeste</Text>
-      </Wrap>
-      <Wrap>
+      </GridItem>
+      <GridItem>
         <WrapItem>
           <Text>50</Text>
           <Text>Países</Text>
@@ -15,7 +15,7 @@ export function ContinentDescription() {
           <Text>50</Text>
           <Text>Países</Text>
         </WrapItem>
-      </Wrap>
-    </Flex>
+      </GridItem>
+    </Grid>
   )
 }
